69a1843691f9e16fc84338baa0222c03ef1281a0
Shafayat Hossain Bayezid <54271386+ShafayatBayezid@users.noreply.github.com>
2022-09-05 18:18:19 +0600

    Components are ready to functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
- 
+import "../styles/App.css";
+import Layout from "./Layout";
+// import Home from "./pages/Home";
+// import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Layout>
+      {/* <Home /> */}
+      {/* <Signup /> */}
+      {/* <Login /> */}
+      {/* <Quiz /> */}
+      <Result />
+    </Layout>
   );
 }
 
